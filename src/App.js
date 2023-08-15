25ec2178b6e956288839de6b9d82eab257d1f2b7
jchoi0406 <jiyongboy@gmail.com>
2023-08-16 01:15:18 +1200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import { Auth } from './components/auth';
+import { db } from './config/firebase';
+import { getDocs, collection, query, limit, where} from 'firebase/firestore';
 function App() {
+  const[albumList, setAlbumList] = useState([]);
+  const albumCollectionsRef = collection(db, "albums");
+  const getAlbumList = async() =>{
+    //READ DATA FROM DB
+    try {
+      const album1 = await getRandomRow();
+      const album2 = await getRandomRow();
+      setAlbumList([album1[0], album2[0]])
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+  async function getRandomRow(){
+    const randomNum = Math.floor(Math.random()*2000)
+    const albumsQuery = query(albumCollectionsRef, where("pos", "==", randomNum),limit(1)); // Limit the query to 2 albums
+    const data = await getDocs(albumsQuery)
+    const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    return filteredData
+  }
+  const displayAlbum = () =>{
+    return(
+      
+      <div>
+            <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+        {albumList.map((alb)=>(
+          <div>
+            <h1>
+              {alb?.album_name}
+            </h1>
+            <h2>
+              {alb?.artist}
+            </h2>
+            <h3>
+              {alb?.avg_rating}
+            </h3>
+
+          </div>
+
+        ))}
+      </div>
+
+      
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth/>
+      <button onClick={getAlbumList}>Matchup!</button>
+      {displayAlbum()}
     </div>
   );
 }
