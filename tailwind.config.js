06eecddf129d6acf326b2d6a6b2af05ac692966d
jchoi0406 <jiyongboy@gmail.com>
2023-08-25 15:26:37 +1200

    fixed scaling for phones

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
-    extend: {},
+    extend: {
+      fontSize:{
+        'xs': '0.75rem',   // Extra Small for phones
+        'sm': '0.875rem',  // Small for phones and small screens
+        'base': '1rem',    // Default font size
+        'lg': '1.125rem',  // Large for tablets and larger screens
+        'xl': '1.25rem',   // Extra Large for desktops
+        '2xl': '1.5rem',   // Extra Extra Large
+      }
+    },
   },
   plugins: [],
 }
