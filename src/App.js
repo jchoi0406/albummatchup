acc9415132a30d3c51cf14165d4d36d9b3353c9b
jchoi0406 <jiyongboy@gmail.com>
2023-08-30 13:50:44 +1200

    added a home page and done some routing

--- src/App.js
+++ src/App.js
@@ -1,79 +1,26 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { Auth } from './components/auth';
-import { db } from './config/firebase';
-import { getDocs, collection, query, limit, where} from 'firebase/firestore';
-import { DisplayAlbum } from './components/Album';
+import React from 'react';
+import Play from './pages/Play';
+import Home from './pages/Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
-  const[albumList, setAlbumList] = useState([]);
-  const albumCollectionsRef = collection(db, "albums");
-  const [score, setScore] = useState(0);
-  const [correct, setCorrect] = useState(null);
-  let prevAlbumNum = null;
-  const [isPhone, setIsPhone] = useState(window.innerWidth <= 480); // breakpoint width for phones 
-  const imgDimension = isPhone ? "w-[100vw] h-[50vh]":"w-[50vw] h-auto"; // heights and widths for images depending on phone or not.
-  const scoreDimension = isPhone ? "w-[15vw] h-[15vw]":"w-[5vw] h-[5vw]"; // heights and widthd for score depending on phone or not.
-  const correctColor = correct ? "bg-green-500": "bg-red-500";
-  useEffect(()=>{  // this useEffect is for checking if the user's screen width changes.
-    function updateIsPhone(){
-      setIsPhone(window.innerWidth<=480);
-    }
-    window.addEventListener("resize", updateIsPhone);
-    return()=>{
-      window.removeEventListener("resize", updateIsPhone);
-    };
-
-  }, [])
-  useEffect(()=>{ // for getting albums from the database
-    const getAlbumList = async() =>{
-      //READ DATA FROM DB
-      try {
-        let album1;
-        let album2;
-
-        if (!correct){
-          album1 = await getRandomRow();
-          album2 = await getRandomRow();
-        }
-        else{
-          console.log(albumList[1]);
-          album1 = [albumList[1]];
-          album2 = await getRandomRow();
-        }
-        setAlbumList([album1[0], album2[0]])
-
-
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getAlbumList();
-  }, [score])
-  
-  async function getRandomRow(){
-    const albumNum = 500;
-    let randomNum;
-    do{
-      randomNum = Math.floor(Math.random()*albumNum)+1;
-    }while (randomNum == prevAlbumNum);
-    prevAlbumNum = randomNum;
-    const albumsQuery = query(albumCollectionsRef, where("pos", "==", randomNum),limit(1)); // Limit the query to 1 album
-    const data = await getDocs(albumsQuery)
-    const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-    return filteredData
-  }
-
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      {/* <Auth/> */}
-      {<DisplayAlbum albumList={albumList} setAlbumList={setAlbumList} score={score} setScore={setScore} setCorrect={setCorrect} imgDimension={imgDimension} isPhone={isPhone}/>}
-      <div className={`${scoreDimension} flex flex-col justify-center text-center rounded-full absolute ${correct === null ? "bg-white": correctColor} text-black text-3xl transition-colors duration-1000`}>
-        {score === null ? 0:score}
-      </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/albummatchup" element={<Home/>}></Route>
+          <Route path="/albummatchup/play" element={<Play/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-
-  );
+  )
 }
+  
+
 
 export default App;
