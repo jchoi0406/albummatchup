79540655cf3db12d28b0eb5431a2fbbcf83a64c1
jchoi0406 <jiyongboy@gmail.com>
2023-08-31 21:29:16 +1200

    fixed home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Play from './pages/Play';
 import Home from './pages/Home';
+
 import {
   BrowserRouter,
   Routes,
@@ -9,12 +10,24 @@ import {
 } from "react-router-dom"
 
 function App() {
+  const [isPhone, setIsPhone] = useState(window.innerWidth <= 480); // breakpoint width for phones 
+
+  useEffect(()=>{  // this useEffect is for checking if the user's screen width changes.
+    function updateIsPhone(){
+      setIsPhone(window.innerWidth<=480);
+    }
+    window.addEventListener("resize", updateIsPhone);
+    return()=>{
+      window.removeEventListener("resize", updateIsPhone);
+    };
+
+  }, [])
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen">
       <BrowserRouter>
         <Routes>
-          <Route path="/albummatchup" element={<Home/>}></Route>
-          <Route path="/albummatchup/play" element={<Play/>}></Route>
+          <Route path="/albummatchup" element={<Home isPhone={isPhone}/>}></Route>
+          <Route path="/albummatchup/play" element={<Play isPhone={isPhone}/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
